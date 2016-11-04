e531d0d4393a73042d0e4dfe021f0b5ecf6f2cb1
Codie Featherstone <cgi-lifebook@outlook.com>
2016-11-04 10:36:48 +0000

    Connecting to App
    Liking to Storyboard and putting in preview view

--- CGI-Chat/CameraVC.swift
+++ CGI-Chat/CameraVC.swift
@@ -8,17 +8,14 @@
 
 import UIKit
 
-class CameraVC: UIViewController {
+class CameraVC: AVCamManualCameraViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
